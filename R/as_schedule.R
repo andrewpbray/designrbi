acc357daae28b8c83a212877b9a70e0596f0ba1b
andrewpbray <andrewbray@berkeley.edu>
2026-02-20 17:30:08 -0800

    update

--- R/as_schedule.R
+++ R/as_schedule.R
@@ -9,20 +9,23 @@
 #' @return A data frame with the schedule of potential outcomes appended as new columns.
 #' @export
 as_schedule <- function(data, treatment, response, append = TRUE, outcome_prefix = "Y") {
+  treatment_nm <- rlang::as_name(rlang::enquo(treatment))
+  response_nm  <- rlang::as_name(rlang::enquo(response))
+
   # Check that the specified columns exist in the data frame
-  if (!all(c(treatment, response) %in% colnames(data))) {
+  if (!all(c(treatment_nm, response_nm) %in% colnames(data))) {
     stop("The specified columns do not all exist in the data frame.")
   }
 
   # Check that the treatment column is a factor
-  if (!is.factor(data[[treatment]])) {
+  if (!is.factor(data[[treatment_nm]])) {
     stop("The treatment column must be a factor.")
   }
   
   # Create the schedule of potential outcomes
   schedule <- tidyr::pivot_wider(data, 
-    names_from = treatment, 
-    values_from = response, 
+    names_from = {{ treatment }}, 
+    values_from = {{ response }}, 
     names_prefix = outcome_prefix,
     names_sort = TRUE)
   
@@ -30,10 +33,10 @@ as_schedule <- function(data, treatment, response, append = TRUE, outcome_prefix
   class(schedule) <- c("schedule", class(schedule))
 
   # Store attributes
-  attr(schedule, "treatment") <- treatment
-  attr(schedule, "response") <- response
-  attr(schedule, "treatment_levels") <- levels(data[[treatment]])
-  attr(schedule, "potential_outcome_cols") <- paste0(outcome_prefix, levels(data[[treatment]]))
+  attr(schedule, "treatment") <- treatment_nm
+  attr(schedule, "response")  <- response_nm
+  attr(schedule, "treatment_levels") <- levels(data[[treatment_nm]])
+  attr(schedule, "potential_outcome_cols") <- paste0(outcome_prefix, levels(data[[treatment_nm]]))
 
   if (append) {
     return(schedule)
